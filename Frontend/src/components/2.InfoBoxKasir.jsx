61272eb7c67fc40d0f9eb57db4063422a82bc020
Rahadian <raha.sovereign@gmail.com>
2024-11-06 21:36:16 +0700

    Updates 4.4, Revs.UI Kasir dan perbaikan info dan tombol pembayaran kasir 3

--- Frontend/src/components/2.InfoBoxKasir.jsx
+++ Frontend/src/components/2.InfoBoxKasir.jsx
@@ -1,4 +1,3 @@
-// 2.InfoBoxKasir.jsx
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
@@ -10,28 +9,39 @@ const InfoBoxKasir = () => {
     if (products.length > logs.length) {
       const newProduct = products[products.length - 1];
       setLogs((prevLogs) => [
-        ...prevLogs,
-        `${new Date().toLocaleString()} Product added successfully: ${newProduct.nama_produk}`,
+        `Product added successfully : ${newProduct.nama_produk} `,
+        ...prevLogs, // Menambahkan log terbaru di bagian atas
       ]);
     } else if (products.length < logs.length) {
       const removedProduct = logs[logs.length - 1].split(': ')[1];
       setLogs((prevLogs) => [
-        ...prevLogs,
-        `${new Date().toLocaleString()} Product removed successfully: ${removedProduct}`,
+        `Product removed successfully : ${removedProduct} `,
+        ...prevLogs, // Menambahkan log terbaru di bagian atas
       ]);
     }
   }, [products]);
 
   return (
-    <div className="card mt-3">
+    <div className="card mt-3" style={{ height: '165px' }}>
       <div className="card-header">
-        <h5 className="mb-0">Information</h5>
+        <h5 className="mb-0">Transaction History</h5>
       </div>
-      <div className="card-body" style={{ maxHeight: '200px', overflowY: 'auto' }}>
+      <div className="card-body" 
+      style={{ 
+        maxHeight: '135px', 
+        overflowY: 'auto', 
+        padding: '0px 10px 0px 0px', /* Urutannya: top, right, bottom, left */
+        }}>
+
         <ul className="list-group list-group-flush">
           {logs.map((log, index) => (
-            <li key={index} className="list-group-item">
-              {log}
+            <li key={index} className="list-group-item" style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <span>{log}</span>
+              <span>
+                {new Date().toLocaleDateString()} 
+                {' | '} {/* Separator antara tanggal dan jam */}
+                {new Date().toLocaleTimeString()}
+              </span>
             </li>
           ))}
         </ul>
--- Frontend/src/components/3.FooterKasir.jsx
+++ Frontend/src/components/3.FooterKasir.jsx
@@ -5,7 +5,7 @@ const FooterKasir = () => {
   return (
     <footer className="bg-light text-center py-1">
       <div className="container">
-      <p className="mb-0">Rahadian | Alpenfohn<sup>TM</sup></p>
+      <p className="mb-0"> Caernarvon | November 2024</p>
       </div>
     </footer>
   );
